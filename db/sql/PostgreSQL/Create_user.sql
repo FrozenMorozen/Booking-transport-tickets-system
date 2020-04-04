5416900bab0d5f7f23a376e5cb0b0d46a6a99d3e
victorgrachev <victorgrachev@mail.ru>
2020-04-04 17:52:44 +0700

    BTTS-12 Создание пользователя и раздача прав

---
+++ db/sql/PostgreSQL/Create_user.sql
@@ -0,0 +1,3 @@
+-- Создание пользователя для бэка
+create user developer
+  with password 'dev09031995';
---
+++ db/sql/PostgreSQL/Grant_table.sql
@@ -0,0 +1,16 @@
+-- Раздача прав пользователю
+grant all on tab_сountry to developer;
+grant all on tab_city to developer;
+grant all on tab_station to developer;
+grant all on tab_distances_station to developer;
+grant all on tab_type_railway_car to developer;
+grant all on tab_seating_railway_car to developer;
+grant all on tab_park_rail_car to developer;
+grant all on tab_train to developer;
+grant all on tab_train_park_car to developer;
+grant all on tab_passenger to developer;
+grant all on tab_history_reservation to developer;
+grant all on tab_train_schedule to developer;
+
+
+
